3093cf161bce9d2c0122376f5b2708544ace4128
Valderlan <valderlanjosr15@gmail.com>
2024-08-16 14:17:09 -0300

    criando o header da aplicação

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,8 @@
-import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-    <UserButton afterSwitchSessionUrl="/" />
+    <div>
+      Página do Dashboard
+    </div>
   )
 }
