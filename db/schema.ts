c408e886759c4edff533fbedf0aee359323a0cb2
Valderlan <valderlanjosr15@gmail.com>
2024-08-21 11:39:27 -0300

    realizado o merge da feature/banco-de-dados para a develop para finalizar a configuração inicial e testar se está funcionando

--- db/schema.ts
+++ db/schema.ts
@@ -4,5 +4,6 @@ export const accounts = pgTable("accounts", {
     id: text("id").primaryKey(),
     plaidId: text("plaid_id"),
     name: text("name").notNull(),
+    email: text("email").notNull(),
     userId: text("user_id").notNull(),
 });
---
+++ drizzle/0002_orange_princess_powerful.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "accounts" ADD COLUMN "email" text NOT NULL;
