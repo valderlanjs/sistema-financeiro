b078308b6d0bb9ef6416232139c6fb7c4d6c4998
Valderlan <valderlanjosr15@gmail.com>
2024-08-22 11:17:35 -0300

    finalizado a criação da api get de contas

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,7 @@
-
-export default function Home() {
+export default function Home() {  
   return (
     <div>
-      Página do Dashboard
+     Home Page
     </div>
-  )
-}
+  );
+};
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,32 +1,17 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
+import accounts from "./accounts";
+
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
 
-app
-  .get("/hello", (c) => {
-    return c.json({ hello: "world" });
-  })
-/*app
-  .get(
-    "/hello", 
-    clerkMiddleware(),
-    (c) => {
-      const auth = getAuth(c);
-
-      if (!auth?.userId) {
-        return c.json({ error: "Não autorizado" });
-      }
-    
-      return c.json({
-        message: "Hello Next.js!",
-        userId: auth.userId,
-      });
-  });*/
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
