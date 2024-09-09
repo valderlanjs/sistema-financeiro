1505c7a8d676873714cbff02422e80a7e89cd359
Valderlan <valderlanjosr15@gmail.com>
2024-09-09 11:58:30 -0300

    criado api de transações com autenticação das contas: - Adiciona endpoints para consulta, criação, atualização e remoção de transações. - integra clerk para autenticação e autorização de usuários. - Com suporte de filtros de data e contas vinculadas a transações. - exclusão em massa das transações. - Integrado com as tabelas de contas e categorias. - Validação com Zod para garantir a integridade dos dados enviados.

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,7 +2,8 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 import accounts from "./accounts";
-import  categories  from "./categories";
+import categories from "./categories";
+import transactions from "./transactions";
 
 
 export const runtime = "edge";
@@ -13,6 +14,8 @@ const app = new Hono().basePath("/api");
 const routes = app
   .route("/accounts", accounts)
   .route("/categories", categories)
+  .route("/transactions", transactions);
+
 
 export const GET = handle(app);
 export const POST = handle(app);
